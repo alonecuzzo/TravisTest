935d114d072b55c21a4c06be5e9d40f43023f6d8
Jabari Bell <jabari.bell@pxlflu.net>
2016-12-01 15:41:24 -0500

    last bits added

--- fastlane/travis.sh
+++ fastlane/travis.sh
@@ -4,3 +4,13 @@ if [["$TRAVIS_PULL_REQUEST] != "false"]]; then
   fastlane test
   exit $?
 fi
+
+if [[ "$TRAVIS_BRANCH" == "master" ]]; then
+  # Travis CI fetches a shallow clone. We use commit count until HEAD for build number. In order to assure that the count is correct we have to unshallow the clone.
+  git fetch --unshallow
+
+  rvm reinstall 2.0.0-p643 --disable-binary
+  fastlane beta
+  exit $?
+fi
+
